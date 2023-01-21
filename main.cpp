4fcc01306538efef613d2b8698cf62c0edf636f5
tesseract241 <dummy@dummy.com>
2023-01-21 20:20:12 +0100

    Added the (optional) timeout parameter

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <atomic>
-void sleep(std::atomic_int32_t &flag, int32_t expected);
+void sleep(std::atomic_int32_t &flag, int32_t expected, int32_t timeout_milliseconds);
 void wake_one(std::atomic_int32_t &flag);
 void wake_all(std::atomic_int32_t &flag);
 #if defined(__linux__)
 
 #include <linux/futex.h>
 #include <sys/syscall.h>
+#include <sys/time.h>
 #include <unistd.h>
 
-void sleep(std::atomic_int32_t &flag, int32_t expected){
+void sleep(std::atomic_int32_t &flag, int32_t expected, int32_t timeout_milliseconds=0){
+    timespec timeout;
+    timespec *timeout_p;
+    if(timeout_milliseconds){
+        timeout.tv_sec  = timeout_milliseconds / 1000;
+        timeout.tv_nsec = (timeout_milliseconds % 1000) * 1000;
+        timeout_p = &timeout;
+    } else timeout_p = NULL;
     while(expected==flag.load()){
-        syscall(SYS_futex, &flag, FUTEX_WAIT_PRIVATE, &expected, NULL, NULL, NULL);
+        syscall(SYS_futex, &flag, FUTEX_WAIT_PRIVATE, &expected, timeout_p, NULL, NULL);
     }
 }
 void wake_one(std::atomic_int32_t &flag, int32_t desired){
@@ -27,9 +35,10 @@ void wake_all(std::atomic_int32_t &flag, int32_t desired){
 #include <Windows.h>
 #pragma comment(lib, "Synchronization.lib")
 
-void sleep(std::atomic_int32_t &flag, int32_t expected){
+void sleep(std::atomic_int32_t &flag, int32_t expected, int32_t timeout_milliseconds){
+    int32_t timeout = timeout_milliseconds ? timeout_milliseconds : INFINITE;
     while(expected==flag.load()){
-        WaitOnAddress(&flag, &expected, 4, INFINITE);
+        WaitOnAddress(&flag, &expected, 4, timeout);
     }
 }
 void wake_one(std::atomic_int32_t &flag, int32_t desired){
